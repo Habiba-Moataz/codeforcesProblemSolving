4077d91b81d39e04390220a14a74832d491ab710
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-09-23 23:23:50 +0200

    Create 282A - Bit++.cpp

---
+++ using c ++ /282A - Bit++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+	int n,x=0;
+	string s;
+	cin>>n;
+	while(n--){
+        cin>>s;
+        if((s[0]== '+')||(s[2])=='+')
+            x++;
+        else
+            x--;
+	}
+	cout<<x<<endl;
+	return 0;
+}

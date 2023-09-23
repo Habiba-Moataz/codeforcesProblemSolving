df696dc60576bb0ada4657a34c2b3b570e2bcc48
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-09-23 19:15:30 +0200

    Create 1760A - Medium Number.cpp

---
+++ using c ++ /1760A - Medium Number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+        cin>>a>>b>>c;
+        if (((a<b)&&(b<c))||((c<b)&&(b<a)))
+            cout<<b<<endl;
+        else if(((b<a)&&(a<c))||((c<a)&&(a<b)))
+            cout<<a<<endl;
+        else
+            cout<<c<<endl;
+	}
+	return 0;
+}

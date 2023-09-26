9aabdb5cac001b4b3893fa0b19439fbeae94a5bd
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-09-26 22:22:35 +0200

    Create 1335A - Candies and Two Sisters.cpp

---
+++ using c ++ /1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long t;
+    long long n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<(n-1)/2<<endl;
+    }
+    return 0;
+}

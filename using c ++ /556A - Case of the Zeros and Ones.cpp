404c4c11081ffd2da95c1d885b207a5715703445
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-10-21 23:51:33 +0200

    Create 556A - Case of the Zeros and Ones.cpp

---
+++ using c ++ /556A - Case of the Zeros and Ones.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main() {
+    int n,x=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for(int i=0;i<n;i++){
+       if(s[i]=='0')
+            x++;
+       else
+            x--;
+    }
+    cout<<abs(x);
+}

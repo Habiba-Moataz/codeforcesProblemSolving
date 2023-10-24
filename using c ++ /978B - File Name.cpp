82f374c1f609eba41ce267023cda3d9929764f19
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-10-24 23:52:08 +0200

    Create 978B - File Name.cpp

---
+++ using c ++ /978B - File Name.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n,c=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for(int i=0;i<n;i++){
+        if((s[i]=='x')&&(s[i+1]=='x')&&(s[i+2]=='x'))
+            c++;
+    }
+    cout<<c;
+    return 0;
+}

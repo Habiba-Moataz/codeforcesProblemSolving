40627e53b9640e76112316890ed4f404477beb13
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-10-27 01:33:31 +0200

    Create 1372A - Omkar and Completion.cpp

---
+++ using c ++ /1372A - Omkar and Completion.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        while(n--){
+            cout<<1<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

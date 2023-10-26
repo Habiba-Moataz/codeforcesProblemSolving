b143650453f759d5712626c2407b4aed297b4692
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-10-27 00:24:35 +0200

    Create 265A - Colorful Stones (Simplified Edition).cpp

---
+++ using c ++ /265A - Colorful Stones (Simplified Edition).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string s,t;
+    int j=0;
+    cin>>s;
+    cin>>t;
+    for(int i=0;i<t.length();i++){
+        if(t[i]==s[j]){
+            j++;
+        }
+    }
+    cout<<j+1<<endl;
+    return 0;
+}

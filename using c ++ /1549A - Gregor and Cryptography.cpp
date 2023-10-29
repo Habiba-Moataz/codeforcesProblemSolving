11373cd59a78470001e132aebe58bfb50eaee229
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-10-29 18:00:55 +0200

    Create 1549A - Gregor and Cryptography.cpp

---
+++ using c ++ /1549A - Gregor and Cryptography.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        long long int p;
+        cin>>p;
+        cout<<2<<"    "<<p-1<<endl;
+    }
+    return 0;
+}

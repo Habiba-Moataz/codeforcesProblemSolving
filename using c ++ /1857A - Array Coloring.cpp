0cb27e3092bc1a9406e056104f178d9d634d20e3
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2023-11-21 22:06:59 +0200

    Create 1857A - Array Coloring.cpp

---
+++ using c ++ /1857A - Array Coloring.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,odd=0,even=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]&1)
+                odd++;
+            else
+                even++;
+        }
+        if(odd&1)
+            cout<<"NO"<<endl;
+        else
+           cout<<"YES"<<endl;
+    }
+    return 0;
+}
